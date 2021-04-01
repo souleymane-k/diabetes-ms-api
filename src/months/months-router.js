e2344f0e0a09e6fd80c8ec9d8fdf25b748eb71d9
souleymane kone <desolo74@gmail.com>
2021-04-01 07:51:52 -0700

    ..month

--- src/months/months-router.js
+++ src/months/months-router.js
@@ -7,18 +7,6 @@ const monthsRouter = express.Router()
 // const bodyParser = express.json()
 const jsonParser = express.json()
 
-const serializeMonth = month => ({
-  id: month.id,
-  // title: xss(bookmark.title),
-  monthName: month.monthName,
-  mealName: month.mealName,
-  result:month.result,
-  date:month.date,
-  description:month.description,
-  dtype:month.dtype
-  // description: xss(month.description),
-  
-})
 
 
 monthsRouter
@@ -26,74 +14,7 @@ monthsRouter
   .get((req, res) => {
     res.json(store.months)
   })
-  // .get((req, res, next) => {
-  //   const monthData = req.app.get('store')
-  //   MonthsService.getAllMonths(monthData)
-  //     .then(months => {
-  //       res.json(months.map(serializeMonth))
-  //     })
-  //     .catch(next)
-  // })
-  // .post(jsonParser, (req, res, next) => {
-  //     //'monthName','mealName', 'result', 'date', 'description', 'dtype'
-  //   const { monthName, mealName, result, date, description,dtype } = req.body
-  //   const newMonth = { monthName, mealName, result, date, description,dtype}
-
-  //   for (const [key, value] of Object.entries(newMonth))
-  //     if (value == null)
-  //       return res.status(400).json({
-  //         error: { message: `Missing '${key}' in request body` }
-  //       })
-
-  //     .then(month => {
-  //       res
-  //         .status(201)
-  //         .location(path.posix.join(req.originalUrl, `/${month.monthName}`))
-  //         .json(serializeMonth(month))
-  //     })
-  //     .catch(next)
-  // })
-
   
-
-  //
-  //
-//   .post(bodyParser, (req, res) => {
-//     for (const field of ['monthName','mealName', 'result', 'date', 'description', 'dtype']) {
-//       if (!req.body[field]) {
-//         return res.status(400).send(`'${field}' is required`)
-//       }
-//     }
-//     if (!Number.isInteger(result) || result < 60 || result > 300) {
-//       return res.status(400).send(`'result' must be a number between  60 and 300`)
-//     }
-//     if (!isWebUri(url)) {
-//       logger.error(`Invalid url '${url}' supplied`)
-//       return res.status(400).send(`'url' must be a valid URL`)
-//     }
-
-//     const month = { id: uuid(), monthName,mealName, result, date, description, dtype }
-
-//     store.months.push(month)
-
-//     res
-//       .status(201)
-//       .location(`http://localhost:8002/months/${month.id}`)
-//       .json(month)
-//   })
-
-// monthsRouter
-// .route('/months/monthName')
-// .get((req,res)=>{
-//   const {monthName} = req.params
-//   let array = [];
-//   for(let i=0; i<store.months.length; i++){
-//     if(months[i].monthName === monthName){
-//       array.push(months[i].monthName)
-//     }
-//   }
-//   res.json(array)
-// })
 
   monthsRouter
   .route('/:id')
@@ -104,42 +25,34 @@ monthsRouter
     res.json(month)
   })
 
+  
+
+
   // monthsRouter
-  // .route('/:monthName')
+  // .route('/:month_id')
+  // .all((req, res, next) => {
+  //   const { month_id } = req.params
+  //   MonthsService.getById(req.app.get('store'), month_id)
+  //     .then(month => {
+  //       if (!month) {
+  //         logger.error(`Month with id ${month_id} not found.`)
+  //         return res.status(404).json({
+  //           error: { message: `Month Not Found` }
+  //         })
+  //       }
+
+  //       res.month = month
+  //       next()
+  //     })
+  //     .catch(next)
+
+  // })
   // .get((req, res) => {
   //   const {monthName} = req.params
   //   const month = store.months.find(i => i.monthName == monthName)
     
-  //   res.json(month)
+  //   res.json(serializeMonth(res.month))
   // })
-
-
-
-  monthsRouter
-  .route('/:month_id')
-  .all((req, res, next) => {
-    const { month_id } = req.params
-    MonthsService.getById(req.app.get('store'), month_id)
-      .then(month => {
-        if (!month) {
-          logger.error(`Month with id ${month_id} not found.`)
-          return res.status(404).json({
-            error: { message: `Month Not Found` }
-          })
-        }
-
-        res.month = month
-        next()
-      })
-      .catch(next)
-
-  })
-  .get((req, res) => {
-    const {monthName} = req.params
-    const month = store.months.find(i => i.monthName == monthName)
-    
-    res.json(serializeMonth(res.month))
-  })
 
 
 
@@ -155,3 +68,38 @@ module.exports = monthsRouter
     //     dtype: month.dtype,
     
     //   }
+
+    // const serializeMonth = month => ({
+//   id: month.id,
+//   // title: xss(bookmark.title),
+//   monthName: month.monthName,
+//   mealName: month.mealName,
+//   result:month.result,
+//   date:month.date,
+//   description:month.description,
+//   dtype:month.dtype
+//   // description: xss(month.description),
+  
+// })
+
+// monthsRouter
+// .route('/months/monthName')
+// .get((req,res)=>{
+//   const {monthName} = req.params
+//   let array = [];
+//   for(let i=0; i<store.months.length; i++){
+//     if(months[i].monthName === monthName){
+//       array.push(months[i].monthName)
+//     }
+//   }
+//   res.json(array)
+// })
+
+// monthsRouter
+  // .route('/:monthName')
+  // .get((req, res) => {
+  //   const {monthName} = req.params
+  //   const month = store.months.find(i => i.monthName == monthName)
+    
+  //   res.json(month)
+  // })
