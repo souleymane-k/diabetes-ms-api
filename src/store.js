462d76fa7dd70180f7304d36020fd43ac2fb23db
souleymane kone <desolo74@gmail.com>
2021-04-01 21:19:22 -0700

    fixing content

--- src/store.js
+++ src/store.js
@@ -1,269 +1,262 @@
 const { v4: uuid } = require('uuid')
 
-const months = [
-    
-    {id:uuid(), monthName:"February",mealName:"Lunch",result:"110 mg/dl",date:"01/03/2021",description:"take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    {id:uuid(), monthName:"March",mealName:"Dinner",result:"140 mg/dl",date:"01/06/2021",description:"take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    { id:uuid(),monthName:"April",mealName:"Breakfast",result:"130 mg/dl",date:"01/05/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    {id:uuid(), monthName:"May",mealName:"Fasting",result:"80 mg/dl",date:"01/09/2021",description:"nothing eaten","dtype":"type 1" },
-    {id:uuid(), monthName:"June",mealName:"Breakfast",result:"120 mg/dl",date:"01/10/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    { id:uuid(),monthName:"July",mealName:"Fasting",result:"75 mg/dl",date:"01/12/2021",description:"nothing eaten","dtype":"type 1" },
-    {id:uuid(), monthName:"August",mealName:"Dinner",result:"140 mg/dl",date:"01/06/2021",description:"take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    { id:uuid(),monthName:"September",mealName:"Breakfast",result:"130 mg/dl",date:"01/05/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    {id:uuid(), monthName:"October",mealName:"Fasting",result:"80 mg/dl",date:"01/09/2021",description:"nothing eaten","dtype":"type 1" },
-    { id:uuid(),monthName:"November",mealName:"Breakfast",result:"120 mg/dl",date:"01/10/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    { id:uuid(),monthName:"December",mealName:"Fasting",result:"75 mg/dl",date:"01/12/2021",description:"nothing eaten","dtype":"type 1" },
-    { id:uuid(),monthName:"January",mealName:"Breakfast",result:"120 mg/dl",date:"01/10/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    { id:uuid(),monthName:"May",mealName:"Fasting",result:"75 mg/dl",date:"01/12/2021",description:"nothing eaten","dtype":"type 1" },
-    { id:uuid(),monthName:"November",mealName:"Breakfast",result:"120 mg/dl",date:"01/10/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
-    { id:uuid(),monthName:"January",mealName:"Fasting",result:"75 mg/dl",date:"01/12/2021",description:"nothing eaten",dtype:"type 1" }
-]
-
-module.exports = { months }
-
-
-
-//////////
-////////
-
 //eslint-disable-next-line
-//          store = {
-//     "months": [
-//       {
-//         id:1,
-//         "name": "January"
-//       },
-//       {
-//         id:2,
-//         "name": "February"
-//       },
-//       {
-//         id:3,
-//         "name": "March"
-//       },
-//       {
-//         id:4,
-//         "name": "April"
-//       },
-//       {
-//         id:5,
-//         "name": "May"
-//       },
-//       {
-//         id:6,
-//         "name": "June"
-//       },
-//       {
-//         id:7,
-//         "name": "July"
-//       },
-//       {
-//         id:8,
-//         "name": "August"
-//       },
-//       {
-//         id:9,
-//         "name": "September"
-//       },
-//       {
-//         id:10,
-//         "name": "October"
-//       },
-//       {
-//         id:11,
-//         "name": "November"
-//       },
-//       {
-//         id:12,
-//         "name": "December"
-//       } 
-//     ],
-//     "results": [
+         module.exports = () => ({
+    "months": [
+      {
+        id:1,
+        "name": "January"
+      },
+      {
+        id:2,
+        "name": "February"
+      },
+      {
+        id:3,
+        "name": "March"
+      },
+      {
+        id:4,
+        "name": "April"
+      },
+      {
+        id:5,
+        "name": "May"
+      },
+      {
+        id:6,
+        "name": "June"
+      },
+      {
+        id:7,
+        "name": "July"
+      },
+      {
+        id:8,
+        "name": "August"
+      },
+      {
+        id:9,
+        "name": "September"
+      },
+      {
+        id:10,
+        "name": "October"
+      },
+      {
+        id:11,
+        "name": "November"
+      },
+      {
+        id:12,
+        "name": "December"
+      } 
+    ],
+    "results": [
 
-//       {
-//         id: uuid(),
-//         monthName:"January",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"01/03/2021",
-//         monthId:1,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"February",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"02/03/2021",
-//         monthId:2,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"March",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"03/03/2021",
-//         monthId:3,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"April",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"04/03/2021",
-//         monthId:4,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"May",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"05/03/2021",
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"June",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"06/03/2021",
-//         monthId:6,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"July",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"07/03/2021",
-//         monthId:7,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"August",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"08/03/2021",
-//         monthId:8,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"September",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"09/03/2021",
-//         monthId:9,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"October",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"10/03/2021",
-//         monthId:10,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"November",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"11/03/2021",
-//         monthId:11,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"December",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"12/03/2021",
-//         monthId:12,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"January",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"01/03/2021",
-//         monthId:1,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//       {
-//         id: uuid(),
-//         monthName:"January",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"01/03/2021",
-//         monthId:1,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//       },
-//     {
-//         id: uuid(),
-//         monthName:"February",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"02/03/2021",
-//         monthId:2,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//     },
-//     {
-//         id: uuid(),
-//         monthName:"February",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"02/03/2021",
-//         monthId:2,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//     },
-//     {
-//         id: uuid(),
-//         monthName:"June",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"06/03/2021",
-//         monthId:6,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//     },
-//     {
-//         id: uuid(),
-//         monthName:"June",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"06/03/2021",
-//         monthId:6,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//     },
-//     {
-//         id: uuid(),
-//         monthName:"July",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"07/03/2021",
-//         monthId:7,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//     },
-//     {
-//         id: uuid(),
-//         monthName:"December",
-//         mealName:"Lunch",result:"110 mg/dl",
-//         date:"12/03/2021",
-//         monthId:12,
-//         description:"take 2hrs after breakfast. 2 bananas",
-//         dtype:"type 1"
-//     }
-//     ]
-//   }
+      {
+        id: uuid(),
+        monthName:"January",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"01/03/2021",
+        monthId:1,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"February",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"02/03/2021",
+        monthId:2,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"March",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"03/03/2021",
+        monthId:3,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"April",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"04/03/2021",
+        monthId:4,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"May",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"05/03/2021",
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"June",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"06/03/2021",
+        monthId:6,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"July",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"07/03/2021",
+        monthId:7,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"August",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"08/03/2021",
+        monthId:8,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"September",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"09/03/2021",
+        monthId:9,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"October",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"10/03/2021",
+        monthId:10,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"November",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"11/03/2021",
+        monthId:11,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"December",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"12/03/2021",
+        monthId:12,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"January",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"01/03/2021",
+        monthId:1,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+      {
+        id: uuid(),
+        monthName:"January",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"01/03/2021",
+        monthId:1,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+      },
+    {
+        id: uuid(),
+        monthName:"February",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"02/03/2021",
+        monthId:2,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+    },
+    {
+        id: uuid(),
+        monthName:"February",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"02/03/2021",
+        monthId:2,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+    },
+    {
+        id: uuid(),
+        monthName:"June",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"06/03/2021",
+        monthId:6,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+    },
+    {
+        id: uuid(),
+        monthName:"June",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"06/03/2021",
+        monthId:6,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+    },
+    {
+        id: uuid(),
+        monthName:"July",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"07/03/2021",
+        monthId:7,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+    },
+    {
+        id: uuid(),
+        monthName:"December",
+        mealName:"Lunch",result:"110 mg/dl",
+        date:"12/03/2021",
+        monthId:12,
+        description:"take 2hrs after breakfast. 2 bananas",
+        dtype:"type 1"
+    }
+    ]
+  })
 
-//   module.exports = {store}
-  
+
+  // const months = [
+    
+//     {id:uuid(), monthName:"February",mealName:"Lunch",result:"110 mg/dl",date:"01/03/2021",description:"take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     {id:uuid(), monthName:"March",mealName:"Dinner",result:"140 mg/dl",date:"01/06/2021",description:"take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     { id:uuid(),monthName:"April",mealName:"Breakfast",result:"130 mg/dl",date:"01/05/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     {id:uuid(), monthName:"May",mealName:"Fasting",result:"80 mg/dl",date:"01/09/2021",description:"nothing eaten","dtype":"type 1" },
+//     {id:uuid(), monthName:"June",mealName:"Breakfast",result:"120 mg/dl",date:"01/10/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     { id:uuid(),monthName:"July",mealName:"Fasting",result:"75 mg/dl",date:"01/12/2021",description:"nothing eaten","dtype":"type 1" },
+//     {id:uuid(), monthName:"August",mealName:"Dinner",result:"140 mg/dl",date:"01/06/2021",description:"take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     { id:uuid(),monthName:"September",mealName:"Breakfast",result:"130 mg/dl",date:"01/05/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     {id:uuid(), monthName:"October",mealName:"Fasting",result:"80 mg/dl",date:"01/09/2021",description:"nothing eaten","dtype":"type 1" },
+//     { id:uuid(),monthName:"November",mealName:"Breakfast",result:"120 mg/dl",date:"01/10/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     { id:uuid(),monthName:"December",mealName:"Fasting",result:"75 mg/dl",date:"01/12/2021",description:"nothing eaten","dtype":"type 1" },
+//     { id:uuid(),monthName:"January",mealName:"Breakfast",result:"120 mg/dl",date:"01/10/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     { id:uuid(),monthName:"May",mealName:"Fasting",result:"75 mg/dl",date:"01/12/2021",description:"nothing eaten","dtype":"type 1" },
+//     { id:uuid(),monthName:"November",mealName:"Breakfast",result:"120 mg/dl",date:"01/10/2021",description:" take 2hrs after breakfast. 2 bananas",dtype:"type 1" },
+//     { id:uuid(),monthName:"January",mealName:"Fasting",result:"75 mg/dl",date:"01/12/2021",description:"nothing eaten",dtype:"type 1" }
+// ]
+
+// module.exports = { months }
   
   
   
