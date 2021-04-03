f90f5e43225a308e6eee53a265df08eec2f79917
souleymane kone <desolo74@gmail.com>
2021-04-03 11:09:28 -0700

    appuse

--- src/server.js
+++ src/server.js
@@ -1,24 +1,67 @@
-const app = require('./app');
-const { PORT } = require('./config');
-const cors = require('cors');
-const {CLIENT_ORIGIN} = require('./config');
-
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+require("dotenv").config();
+const knex=require('knex');
+const app =require('./app');
+const { PORT, DATABASE_URL } = require('./config');
+const pg = require('pg');
 
 
+pg.defaults.ssl = process.env.NODE_ENV ==="production"?
+{rejectUnauthorized:false}:false
+const db = knex({
+  client:"pg",
+  connection:DATABASE_URL,
+  ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+});
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+
+app.set("db", db);
+
+app.listen(PORT,() =>{
+  console.log(`server listening at http://localhost:${PORT}`);
 })
 
-module.exports = {app};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = require('./app');
+// const { PORT } = require('./config');
+// const cors = require('cors');
+// const {CLIENT_ORIGIN} = require('./config');
+
+// app.use(
+//     cors({
+//         origin: CLIENT_ORIGIN
+//     })
+// );
+
+
+
+//  app.get('/api/*', (req, res) => {
+//    res.json({ok: true});
+//  });
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`)
+// })
+
+// module.exports = {app};
 
 
 
